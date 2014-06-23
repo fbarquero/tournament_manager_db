330ca7f0353550bd1baca2fcbadcd1eec765fe43
fbarquero <fbarquero86@gmail.com>
2014-06-23 17:17:41 -0600

    adding insert script

---
+++ tournament_manager_db_insert_script.sql
@@ -0,0 +1,45 @@
+use tournament_manager_db;
+
+-- Belt info --
+INSERT INTO belt (description) values ('Blanco');
+INSERT INTO belt (description) values ('Amarillo');
+INSERT INTO belt (description) values ('Verde');
+INSERT INTO belt (description) values ('Azul');
+INSERT INTO belt (description) values ('Rojo');
+INSERT INTO belt (description) values ('Negro');
+
+-- weight_category info --
+INSERT INTO weight_category (name) values ('Fin');
+INSERT INTO weight_category (name) values ('Fly');
+INSERT INTO weight_category (name) values ('Bantam');
+INSERT INTO weight_category (name) values ('Feather');
+INSERT INTO weight_category (name) values ('Light');
+INSERT INTO weight_category (name) values ('Welter');
+INSERT INTO weight_category (name) values ('Light Middle');
+INSERT INTO weight_category (name) values ('Middle');
+INSERT INTO weight_category (name) values ('Light Heavy');
+INSERT INTO weight_category (name) values ('Heavy');
+INSERT INTO weight_category (name) values ('-54');
+INSERT INTO weight_category (name) values ('-58');
+INSERT INTO weight_category (name) values ('-63');
+INSERT INTO weight_category (name) values ('-68');
+INSERT INTO weight_category (name) values ('-74');
+INSERT INTO weight_category (name) values ('-80');
+INSERT INTO weight_category (name) values ('-87');
+INSERT INTO weight_category (name) values ('+87');
+INSERT INTO weight_category (name) values ('-46');
+INSERT INTO weight_category (name) values ('-49');
+INSERT INTO weight_category (name) values ('-53');
+INSERT INTO weight_category (name) values ('-62');
+INSERT INTO weight_category (name) values ('-67');
+INSERT INTO weight_category (name) values ('-73');
+INSERT INTO weight_category (name) values ('+73');
+
+-- age_category --
+INSERT INTO age_category (name) values ('Infantil');
+INSERT INTO age_category (name) values ('Cadete');
+INSERT INTO age_category (name) values ('Junior');
+INSERT INTO age_category (name) values ('Senior');
+INSERT INTO age_category (name) values ('Ejecutivo');
+
+
